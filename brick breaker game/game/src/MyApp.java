261e83c9b6e6ddb2a652333c937db84d92e97c8f
Joyee2004 <128905872+Joyee2004@users.noreply.github.com>
2023-11-26 12:04:25 +0530

    Update MyApp.java

--- brick breaker game/game/src/MyApp.java
+++ brick breaker game/game/src/MyApp.java
@@ -1,15 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import javax.swing.JFrame;
 
-/**
- *
- * @author chinm
- */
 public class MyApp {
     public static void main(String[] args) {
         JFrame obj = new JFrame();
