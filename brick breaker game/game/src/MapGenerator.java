211e2482d719bb113c7163d8e158c4f37aecb2ca
Joyee2004 <128905872+Joyee2004@users.noreply.github.com>
2023-11-26 12:02:07 +0530

    Update MapGenerator.java

--- brick breaker game/game/src/MapGenerator.java
+++ brick breaker game/game/src/MapGenerator.java
@@ -1,20 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
-/**
- *
- * @author chinm
- */
 public class MapGenerator {
-     public int map[][];
+    public int map[][];
     public int bricksWidth;
     public int bricksHeight;
     public MapGenerator(int row , int col){
@@ -31,7 +20,7 @@ public class MapGenerator {
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[0].length; j++) {
                 if (map[i][j] > 0) {
-                    g.setColor(Color.red);
+                    g.setColor(Color.PINK);
                     g.fillRect(j * bricksWidth + 80, i * bricksHeight + 50, bricksWidth, bricksHeight);
 
                     g.setStroke(new BasicStroke(3));
