9600719d2da9abb5a7b94318203a259693b09508
Sayali <124203472+say2410@users.noreply.github.com>
2023-12-02 22:08:08 +0530

    Update MyApp.java

--- brick breaker game/game/src/MyApp.java
+++ brick breaker game/game/src/MyApp.java
@@ -5,7 +5,7 @@ public class MyApp {
         JFrame obj = new JFrame();
         GamePlay gameplay = new GamePlay();
         obj.setBounds(10,10,700,600);
-        obj.setTitle("BrickBreaker");
+        obj.setTitle("Breakout Ball");
         obj.setResizable(false);
         obj.setVisible(true);
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
